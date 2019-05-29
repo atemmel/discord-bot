fc10411940a021892575b4dd1b4d440b1ee9bfbc
atemmel <atemmel@gmail.com>
2019-05-29 07:09:06 +0200

    Created shell of program

--- main.go
+++ main.go
@@ -7,8 +7,31 @@ import (
 
 func main() {
 
-	discord, err := discordgo.New("Bot " + "token")
+	discord, err := discordgo.New("Bot " + "dummy token")
 
-	fmt.Println(err)
-	fmt.Println(discord)
+	if err != nil {
+		fmt.Println("Error creating bot,", err)
+		return
+	}
+
+	discord.AddHandler(messageCreate)
+
+	err = discord.Open()
+
+	if err != nil {
+		fmt.Println("Error opening connection:", err)
+		return
+	}
+
+	fmt.Println("Bot is now running")
+
+	discord.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
 }
