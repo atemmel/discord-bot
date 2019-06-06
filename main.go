5a191c20af3ecb3ec1e114ff903f12fd06e4dc5c
atemmel <temmeladam@gmail.com>
2019-06-07 00:09:04 +0200

    Implemented a basic command

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"flag"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"math/rand"
+	"time"
 )
 
 var (
 	Token string
+	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 )
+
+func RandStringRunes(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letterRunes[rand.Intn(len(letterRunes))]
+    }
+    return string(b)
+}
 
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot token")
 	flag.Parse()
+	rand.Seed(time.Now().Unix() )
 }
 
 func main() {
@@ -35,6 +50,10 @@ func main() {
 
 	fmt.Println("Bot is now running")
 
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
 	discord.Close()
 }
 
@@ -44,4 +63,25 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	if m.Content == "russian roulette" {
+		members, err := s.GuildMembers(m.GuildID, "", 90)
+
+		if( err != nil) {
+			return
+		}
+
+		member := members[rand.Intn(len(members) ) ]
+
+		length := 3 + rand.Intn(32 - 3)
+
+		name := RandStringRunes(length)
+
+		fmt.Println(member.User, "will have their name changed to", name)
+
+		err = s.GuildMemberNickname(m.GuildID, member.User.ID, name)
+
+		if( err != nil) {
+			fmt.Println(err)
+		}
+	}
 }
